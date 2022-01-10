b89d48e57c9846b105feede589066dd2029d3508
TrinhNV <trinhnguyen959@gmail.com>
2022-01-10 21:02:04 +0700

    3. Builder

---
+++ java-design-patterns/src/com/java/builder/PersonBuilder.java
@@ -0,0 +1,55 @@
+package com.java.builder;
+
+class Person {
+	public String name;
+	public String position;
+
+	@Override
+	public String toString() {
+		return "PersonBuilder{" +
+				"name='" + name + '\'' +
+				", position='" + position + '\'' +
+				'}';
+	}
+}
+
+class PersonBuilder<T extends PersonBuilder<T>> {
+	protected Person person = new Person();
+
+	public T withName(String name) {
+		person.name = name;
+		return (T) this;
+	}
+
+	public Person build() {
+		return person;
+	}
+
+	protected T self() {
+		return (T) this;
+	}
+}
+
+class EmployeeBuilder extends PersonBuilder<EmployeeBuilder> {
+	public EmployeeBuilder worksAt(String position) {
+		person.position = position;
+		return self();
+	}
+
+	@Override
+	protected EmployeeBuilder self() {
+		return this;
+	}
+}
+
+class ApplicationPersonBuilder {
+	public static void main(String[] args) {
+		EmployeeBuilder builder = new EmployeeBuilder();
+		Person person = builder
+				.withName("Alexander")
+				.worksAt("King")
+				.build();
+
+		System.out.println(person);
+	}
+}
