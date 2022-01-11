c1758ecade4dfc85d5f8c0aac0270263d3f6276a
TrinhNV <trinhnguyen959@gmail.com>
2022-01-11 21:40:48 +0700

    5. Laziness and Thread Safety

---
+++ java-design-patterns/src/com/java/singleton/LazySingleton.java
@@ -0,0 +1,30 @@
+package com.java.singleton;
+
+public class LazySingleton {
+	private static LazySingleton instance;
+
+	private LazySingleton() {
+		System.out.println("Initializing LazySingleton...");
+	}
+
+	// fix synchronized for multi-thread
+	public static synchronized LazySingleton getInstance() {
+		if (instance == null) {
+			instance = new LazySingleton();
+		}
+		return instance;
+	}
+
+	// double check block
+	public static LazySingleton getInstanceDoubleChecked() {
+		if (instance == null) {
+			synchronized (LazySingleton.class) {
+				if (instance == null) {
+					instance = new LazySingleton();
+				}
+			}
+		}
+
+		return instance;
+	}
+}
