5d0cfad7454b5cb704680e14329333feb3d07349
TrinhNV <trinhnguyen959@gmail.com>
2022-01-11 21:32:04 +0700

    Static block singleton

---
+++ java-design-patterns/src/com/java/singleton/StaticBlockSingleton.java
@@ -0,0 +1,30 @@
+package com.java.singleton;
+
+import java.io.File;
+import java.io.IOException;
+
+public class StaticBlockSingleton {
+	private StaticBlockSingleton() throws IOException {
+		System.out.println("Singleton is initializing");
+//		File.createTempFile(".", ".");
+	}
+	private static StaticBlockSingleton instance;
+	static {
+		try {
+			instance = new StaticBlockSingleton();
+		} catch (Exception e) {
+			System.err.println("Fail to create singleton");
+		}
+		System.out.println("Singleton create successful!");
+	}
+
+	public static StaticBlockSingleton getInstance() {
+		return instance;
+	}
+}
+
+class StaticApplication {
+	public static void main(String[] args) {
+		StaticBlockSingleton.getInstance();
+	}
+}
